a889b69c0e24fc9cb5baa1781d14836345365fba
abhishek30920 <abhishek309200@gmail.com>
2025-02-08 17:58:10 +0530

    issue solved

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,7 @@ import path from "path";
 import cors from "cors";
 import fs from "fs";
 import { createServer } from "http";
-import cron from "node-cron";
+
 
 
 
@@ -21,7 +21,7 @@ import { initializeSocket } from './lib/socket.js';
 
 dotenv.config();
 
-const __dirname = path.resolve();
+
 const app = express();
 const PORT = process.env.PORT;
 
@@ -37,32 +37,9 @@ app.use(
 
 app.use(express.json()); // to parse req.body
 app.use(clerkMiddleware()); // this will add auth to req obj => req.auth
-app.use(
-	fileUpload({
-		useTempFiles: true,
-		tempFileDir: path.join(__dirname, "tmp"),
-		createParentPath: true,
-		limits: {
-			fileSize: 10 * 1024 * 1024, // 10MB  max file size
-		},
-	})
-);
+
 initializeSocket(httpServer);
 // cron jobs
-const tempDir = path.join(process.cwd(), "tmp");
-cron.schedule("0 * * * *", () => {
-	if (fs.existsSync(tempDir)) {
-		fs.readdir(tempDir, (err, files) => {
-			if (err) {
-				console.log("error", err);
-				return;
-			}
-			for (const file of files) {
-				fs.unlink(path.join(tempDir, file), (err) => {});
-			}
-		});
-	}
-});
 
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
@@ -71,17 +48,9 @@ app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-	});
-}
+
 
 // error handler
-app.use((err, req, res, next) => {
-	res.status(500).json({ message: err.message });
-});
 
 httpServer.listen(PORT, () => {
 	console.log("Server is running on port " + PORT);
